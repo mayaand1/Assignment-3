13019fa107094b52ecc04f412556391be48c3973
mayaand1 <maya.anderson1@ontariotechu.net>
2024-11-24 23:50:15 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Database connection error:", err));
